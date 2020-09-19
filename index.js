959af51f550de64c6d75d118cd988e2134dfc3ea
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-19 06:48:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(resp => renderBooks(resp));
+}
+function renderBooks(books) {
+  const main = document.querySelector('main')
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  });
+}
+document.addEventListener('DOMContentLoaded', function() {
+  submitData();
+})
